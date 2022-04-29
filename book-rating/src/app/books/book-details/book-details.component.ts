f0caf86c802901d5b79ff5e94e13c39fde08a6f3
Ferdinand Malcher <ferdinand@malcher.media>
2022-04-29 11:24:03 +0200

    Routing: Parameter / Detailseite

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Book } from '../shared/book';
+import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
   selector: 'br-book-details',
@@ -7,7 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookDetailsComponent implements OnInit {
 
-  constructor() { }
+  book?: Book;
+
+  constructor(private route: ActivatedRoute, private bs: BookStoreService) {
+    // PULL / Synchroner Weg
+    // const isbn = this.route.snapshot.paramMap.get('isbn'); // path: 'books/:isbn'
+
+    // PUSH / Asynchroner Weg
+    // TODO: Verschachtelte Subscriptions vermeiden (wegen Race Condition!)
+    this.route.paramMap.subscribe(params => {
+      const isbn = params.get('isbn')!; // Non-Null Assertion (hier Ausnahmefall!)
+
+      this.bs.getSingle(isbn).subscribe(book => {
+        this.book = book;
+      });
+    });
+  }
+
+  /*
+  HTTP: BookStoreService.getSingle()
+  Buch anzeigen
+  */
 
   ngOnInit(): void {
   }
