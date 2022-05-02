fc54fcd8f605a6c615e2f5469275a9fb55762275
Ferdinand Malcher <ferdinand@malcher.media>
2022-05-02 10:50:46 +0200

    Forms: Validierung und Feedback

--- book-rating/src/app/books/book-create/book-create.component.ts
+++ book-rating/src/app/books/book-create/book-create.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'br-book-create',
@@ -9,16 +9,39 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class BookCreateComponent implements OnInit {
 
   bookForm = new FormGroup({
-    isbn: new FormControl(''),
-    title: new FormControl(''),
+    isbn: new FormControl('', [
+      Validators.required,
+      Validators.minLength(10),
+      Validators.maxLength(13)
+    ]),
+    title: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(50)
+    ]),
     description: new FormControl(''),
-    rating: new FormControl(1),
-    price: new FormControl(0),
+    rating: new FormControl(1, [
+      Validators.min(1),
+      Validators.max(5),
+    ]),
+    price: new FormControl(0, Validators.min(0)),
   });
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  isInvalid(controlName: string): boolean {
+    const control = this.bookForm.get(controlName);
+    return !!control && control.touched && control.invalid;
+    // return control ? control.touched && control.invalid : false;
+    // return !!(control?.touched && control?.invalid);
+  }
+
+  hasError(controlName: string, errorCode: string): boolean {
+    const control = this.bookForm.get(controlName);
+    // return !!control && !!control.getError(errorCode) && control.touched;
+    return !!control && control.hasError(errorCode) && control.touched;
   }
 
 }
@@ -27,10 +50,10 @@ export class BookCreateComponent implements OnInit {
 
 /*
 TODO:
-- Submit-Button
+- Submit-Button ✅
 - nur abschicken, wenn alle Felder gültig
-- Validierung
-- Hinweismeldungen
+- Validierung ✅
+- Hinweismeldungen ✅
 
 - HTTP
 - bei Erfolg: Wegnavigieren z.B. zur Detailseite
