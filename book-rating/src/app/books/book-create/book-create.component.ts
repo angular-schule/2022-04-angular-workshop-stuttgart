5ee649c49db43ddba4f6bc4a0b2fba4b09651e6a
Ferdinand Malcher <ferdinand@malcher.media>
2022-05-02 11:33:32 +0200

    Forms: Abschicken und HTTP

--- book-rating/src/app/books/book-create/book-create.component.ts
+++ book-rating/src/app/books/book-create/book-create.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Book } from '../shared/book';
+import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
   selector: 'br-book-create',
@@ -26,22 +29,32 @@ export class BookCreateComponent implements OnInit {
     price: new FormControl(0, Validators.min(0)),
   });
 
-  constructor() { }
+  constructor(private bs: BookStoreService, private router: Router) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   isInvalid(controlName: string): boolean {
     const control = this.bookForm.get(controlName);
     return !!control && control.touched && control.invalid;
     // return control ? control.touched && control.invalid : false;
     // return !!(control?.touched && control?.invalid);
+
+    control.
   }
 
   hasError(controlName: string, errorCode: string): boolean {
     const control = this.bookForm.get(controlName);
     // return !!control && !!control.getError(errorCode) && control.touched;
     return !!control && control.hasError(errorCode) && control.touched;
+  }
+
+  submitForm() {
+    const book: Book = this.bookForm.value;
+
+    this.bs.create(book).subscribe(receivedBook => {
+      this.router.navigate(['/books', receivedBook.isbn]); // [routerLink]="['/books', book.isbn]"
+      // this.router.navigateByUrl('/books'); // routerLink="/books"
+    });
   }
 
 }
